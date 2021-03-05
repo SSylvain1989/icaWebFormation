1286b04526eeab3299a91ceee8a4cc68e97dfa08
SSylvain1989 <sylvain.sigonnez@gmail.com>
2021-03-05 19:06:51 +0100

    voir et télécharger les fiches formations à partir du tableau ok

--- components/formations/formations.js
+++ components/formations/formations.js
@@ -4,33 +4,33 @@ import "gridjs/dist/theme/mermaid.css";
 import styles from './formations.module.scss';
 
 //==import pdf
-import learning1 from '../../public/base.pdf';
-import moncv from '../../public/cv_sylvain_sigonnez.pdf';
-import quisommes from '../../public/quisommesnous1.jpeg';
-import test from '../../public/ica-logo.png';
-// import test from '../../public/ica-logo.png';
-
-
-
-
+import moncv from './../../public/pdf/cv_sylvain_sigonnez.pdf';
 
 export default function Formations ({data, typeFormation}) {
 console.log(data);
 
 const columns = ['Bureaux d’étude','Maintenance','Exploitation', typeFormation,'Durée','Répartition en modules' ,
 { 
-  name: 'fiches formations',
-  formatter: (_, row) => html(`<a href='mailto:${row.cells[3].data}'>télécharger la fiche</a>`)
-},];
+  name: 'Télécharger la fiche',
+  formatter: (_, row) => html(`<a href='/pdf/${row.cells[3].data}.pdf' download='ica-${row.cells[3].data}' >télécharger la fiche</a>`)
+},
+{ 
+  name: 'Voir la fiche',
+  formatter: (_, row) => html(`<a href='/pdf/${row.cells[3].data}.pdf' target='_blank' >voir la fiche</a>`)
+},
+];
 
+
+const text = "/pdf/cv_sylvain_sigonnez.pdf";
   return(  
   <div className={styles.formations}>
-      <a className="cv" href={moncv} download="cv sylvain sigonnez">pdf</a>
+      <a  href={text} target="_blank" download="quisommesnous">pdf</a>
+      <a  href={moncv} target="_blank" download="quisommesnous">pdf</a>
       <br/>
-      <a className="cv" href={quisommes} download="photojpeg">jpeg</a>
+      <a  href="/pdf/Les responsabilités en ATEX.pdf" download="photojpeg">jpeg</a>
       <br/>
 
-      <a className="cv" href={test} download="photopng">png</a>
+      <a  href="/images/ica-logo.png" download="photopng">png</a>
 
 
 
@@ -49,4 +49,8 @@ const columns = ['Bureaux d’étude','Maintenance','Exploitation', typeFormatio
 
 }
 
+
+import test from '../../public/images/ica-logo.png';
+
+import Image from 'next/image';
 
